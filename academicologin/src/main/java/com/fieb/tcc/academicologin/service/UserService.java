f7f23b0397e03b6c6febe4cc1dd7cba4887fa6ff
rm88486 <willian-ssantos.2012@hotmail.com>
2023-03-06 22:10:50 -0300

    Camada movel parcialmente concluida

---
+++ academicologin/src/main/java/com/fieb/tcc/academicologin/service/UserService.java
@@ -0,0 +1,7 @@
+package com.fieb.tcc.academicologin.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+}
